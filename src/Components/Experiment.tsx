850af72ab8b4bcaed7803c0eeb2cea68202bc3ac
Krishna-Arjun <krishnaarjunkl@gmail.com>
2023-07-29 13:00:00 +0530

    MF-69 Fav icon  added

--- src/Components/Experiment.tsx
+++ src/Components/Experiment.tsx
@@ -5,7 +5,7 @@ const Experiment = () => {
     <div className="w-full py-5 font-poppins px-20">
       <div className="flex flex-col space-y-5">
         <div className="w-fit text-xl font-bold  hover:underline decoration-blue-500 decoration-2 underline-offset-4">
-          <p className="text-gray-600">EXPERIMENT</p>
+          <p className="text-gray-600">EXPERIMENTS</p>
         </div>
 
         <div className="flex h-96 justify-center items-center grow">
